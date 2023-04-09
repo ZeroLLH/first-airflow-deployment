73109eca8641cce67741f92059694d7ae5f304f9
Ethan L H Liang <342351775@qq.com>
2023-04-09 20:24:54 +0800

    Run tasks in parallel

--- dags/our_first_dag.py
+++ dags/our_first_dag.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='our_first_dag_v2',
+    dag_id='our_first_dag_v3',
     default_args=default_args,
     description='This is our first dag that we write',
     start_date=datetime(2023, 4, 9, 2),
@@ -26,4 +26,10 @@ with DAG(
         bash_command="echo hey, I am task2 and will be running after task 1!"
     )
 
+    task3 = BashOperator(
+        task_id='third_task',
+        bash_command="echo hey, I am task3 and will be running after task 1 at the same time as task2! "
+    )
+
     task1.set_downstream(task2)
+    task1.set_downstream(task3)
