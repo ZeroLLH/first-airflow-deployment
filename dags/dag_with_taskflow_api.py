365b2da9177340175f9e0e5cf0e82c734bb02221
Ethan L H Liang <342351775@qq.com>
2023-04-09 21:23:17 +0800

    Create 3rd DAG using decorators - with multiple outputs

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -8,7 +8,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-@dag(dag_id='dag_with_taskflow_api_v01',
+@dag(dag_id='dag_with_taskflow_api_v02',
      default_args=default_args,
      start_date=datetime(2023,4,8),
      schedule_interval='@daily')
@@ -26,12 +26,14 @@ def hello_world_etl():
         return 19
 
     @task()
-    def greet(name, age):
-        print(f"Hello World! My name is {name} "
+    def greet(first_name, last_name, age):
+        print(f"Hello World! My name is {first_name} {last_name} "
               f"and I am {age} years old!")
 
-    name = get_name()
+    name_dict = get_name()
     age = get_age()
-    greet(name=name, age=age)
+    greet(first_name=name_dict['first_name'],
+          last_name=name_dict['last_name'],
+          age=age)
 
 greet_dag = hello_world_etl()
