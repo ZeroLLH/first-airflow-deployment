6fdb18011ea2853138891861346d72e3524c184d
Ethan L H Liang <342351775@qq.com>
2023-04-09 21:07:28 +0800

    Create 2nd DAG using python operator - with multiple tasks provided multiple arguments

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -9,9 +9,10 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(age, ti):
+def greet(ti):
     first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
     last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
     print(f"Hello World! My name is {first_name} {last_name}, "
           f"and I am {age} years old!")
 
@@ -19,17 +20,19 @@ def get_name(ti):
     ti.xcom_push(key='first_name', value='Jerry')
     ti.xcom_push(key='last_name', value='Fridman')
 
+def get_age(ti):
+    ti.xcom_push(key='age', value=19)
+
 with DAG(
     default_args=default_args,
-    dag_id='our_dag_with_python_operator_v05',
+    dag_id='our_dag_with_python_operator_v06',
     description='Our first dag using python operator',
     start_date=datetime(2023,4,8),
     schedule_interval='@daily'
 ) as dag:
     task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet,
-        op_kwargs={'age': 20}
+        python_callable=greet
     )
 
     task2 = PythonOperator(
@@ -37,4 +40,9 @@ with DAG(
         python_callable=get_name
     )
 
-    task2 >> task1
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+
+    [task2, task3] >> task1
